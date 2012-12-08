f2bfead2fadee4dfc9a5acff8efc2ea8471efed2
Jhoseph Araujo <j.garibald@gmail.com>
2012-12-08 16:21:39 -0300

    primeiro commit

---
+++ src/com/jhoseph/usandogit/Main.java
@@ -0,0 +1,15 @@
+package com.jhoseph.usandogit;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Olá Mundo GIT, mais uma vez...");
+
+	}
+
+}
